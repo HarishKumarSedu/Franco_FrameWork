cf490d7491b860a3e8c8f4c8b7be842d2d558984
harish <harkum@inventvm.com>
2023-12-15 17:22:13 +0100

    trimming data updated for 34,25,26

--- inventvmScripts/example_inventm_test_script.py
+++ inventvmScripts/example_inventm_test_script.py
@@ -97,11 +97,11 @@ startup = Startup(dut=dut)
 # dut.SIMULINK_MODEL.GAIN_CONFIG3.IBUS_GAIN.value = 0x460
 # QuickCheck(dut=dut)
 loadTrim = LoadTrims(dut=dut,path='json/TrimmingResults_10_10.json',chipid=10)
-# loadTrim.loadTrims()
+loadTrim.loadTrims()
 # trim = Trim(test_station=test_station,DFT_path='data/DFTInstructions_new.json',loadTrim=loadTrim)
 # efficiency = Efficiency(dut=dut)
 # cflychar = CflyChar(dut=dut)
-innerloop = InnerLoop(dut=dut,loadtrims=loadTrim)
+# innerloop = InnerLoop(dut=dut,loadtrims=loadTrim)
 #charecterization
 # char = AONChar(dut=dut)
 
